16deb2fb053d980b9c329723af6cae92c9e6e8af
MRomero <romerolaguado@gmail.com>
2022-05-06 11:45:31 -0500

    Modificación
    Modificando el código del programa del primer archivo creado.

--- Primer Juego/invasion_alienigena.py
+++ Primer Juego/invasion_alienigena.py
@@ -3,20 +3,21 @@ import sys
 import pygame
 # Cada elemento del videojuego es una superficie. Ej: los enemigos, el jugador, entre otros.
 
+from configuraciones import Configuraciones
+
 def run_game():
+    # Inicializar el juego, las configuraciones y crear un objeto pantalla
     pygame.init()
+    ai_configuraciones = Configuraciones() # Se crea una instancia de configuraciones
     # Se crea una pantalla de visualizacion llamada "pantalla"
-    # pixeles de ancho: 800; pixeles de alto: 600
-    pantalla = pygame.display.set_mode((800, 600))
+    pantalla = pygame.display.set_mode(
+        (ai_configuraciones.screen_width, ai_configuraciones.screen_height))
     # Se digita el nombre del videojuego:
     pygame.display.set_caption("Invasión alienígena")
-    
-    # Cambiar el color de fondo por defecto (negro) de la pantalla:
-    bg_color = (230, 230, 230) # Se utiliza los colores RGB
-
 
     # Bucle de activación del videojuego:
     while True:
+        # Escuchar eventos de teclado o de ratón
         # El bucle gestiona los eventos y código que actualiza la pantalla
         # Evento: acción que realiza un usuario mientas juega.
         for event in pygame.event.get():
@@ -24,7 +25,7 @@ def run_game():
                 sys.exit()
 
         # Volver a dibujar la pantalla durante cada pasada por el bucle
-        pantalla.fill(bg_color)
+        pantalla.fill(ai_configuraciones.bgcolor)
         # Hacer visible la pantalla dibujada más reciente
         pygame.display.flip()
 
