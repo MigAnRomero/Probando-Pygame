03f5a890d0cdbb0822ef25617d38198972a5555c
MRomero <romerolaguado@gmail.com>
2022-05-06 11:44:50 -0500

    Configuraciones
    Se creó un nuevo archivo.

---
+++ Primer Juego/configuraciones.py
@@ -0,0 +1,8 @@
+class Configuraciones(object):
+    """Sirve para almacenar todas las congiguraciones de Invasión Alienígena"""
+
+    def __init__(self):
+        """Inicializa las configuraciones del juego"""
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bgcolor = (230, 230, 230)
