601043de13e52e5b4dc23834409d0b18a0ff8e6d
MRomero <romerolaguado@gmail.com>
2022-05-11 11:32:03 -0500

    Archivo nave
    Se ha creado el archivo nave para crear la clase Nave que, sirve para gestionar el comportamiento de la misma en la pantalla.

---
+++ Primer Juego/nave.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Nave():
+    """Sirve para gestionar el comportamiento de la nave"""
+
+    def __init__(self, pantalla):
+        """Inicializa la nave y establece su posición de partida"""
+        
+        self.pantalla = pantalla
+
+        # Carga la imagen de la nave y obtiene su rect
+        self.imagen = pygame.image.load("img/nave_uno.bmp")
+        self.rect = self.imagen.get_rect()
+        self.pantalla_rect = pantalla.get_rect()
+
+        # Empieza cada nueva nave en la parte inferior central de la pantalla
+        self.rect.centerx = self.pantalla_rect.centerx
+        self.rect.bottom = self.pantalla_rect.bottom
+
+    def blitme(self):
+        """Dibuja la nave en su ubicación actual"""
+        self.pantalla.blit(self.imagen, self.rect)
