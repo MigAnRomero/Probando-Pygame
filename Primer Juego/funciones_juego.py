942262ef53d00990d3a8113f4678cda198cb8a8e
Miguel Romero <romerolaguado@gmail.com>
2022-05-13 21:17:36 -0500

    Refactorizar código

--- Primer Juego/funciones_juego.py
+++ Primer Juego/funciones_juego.py
@@ -2,6 +2,20 @@ import sys
 
 import pygame
 
+def verificar_eventos_keydown(event, nave):
+    """Responde a las pulsaciones de teclas"""
+    if event.key == pygame.K_RIGHT:
+        nave.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        nave.moving_left = True
+
+def verificar_eventos_keyup(event, nave):
+    """Responde a las pulsaciones de teclas"""
+    if event.key == pygame.K_RIGHT:
+        nave.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        nave.moving_left = False
+            
 def verificar_eventos(nave):
     """Responde a las pulsaciones de teclas y los eventos del ratón"""
     # Escuchar eventos de teclado o de ratón
@@ -11,15 +25,11 @@ def verificar_eventos(nave):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                nave.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                nave.moving_left = True
+            verificar_eventos_keydown(event, nave)
+            
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                nave.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                nave.moving_left = False
+            verificar_eventos_keyup(event, nave)
+            
 
 def actualizar_pantalla(ai_configuraciones, pantalla, nave):
     """Actualiza las imágenes en la pantalla y pasa a la nueva pantalla"""
